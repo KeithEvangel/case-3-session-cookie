4c4949c3fcf3b9d61d9756c0c7113212b92101e8
KeithEvangel <167418205+KeithEvangel@users.noreply.github.com>
2024-04-23 01:15:57 +0700

    logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,16 @@
 <?php
+// Memulai atau melanjutkan sesi yang sudah ada
 session_start();
+
+// Menghapus semua data yang disimpan dalam sesi
 session_unset();
+
+// Menghancurkan sesi yang sedang berlangsung
 session_destroy();
 
+// Mengarahkan pengguna kembali ke halaman utama
 header("Location: index.php");
+
+// Menghentikan eksekusi skrip setelah pengalihan header dilakukan
 exit();
+?>
